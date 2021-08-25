d4b262053d0dbcb11b0f19b051d3532764adde61
Ge6ben <dl22gesh40@gmail.com>
2021-08-25 14:58:14 +0300

    check

--- src/reducer.js
+++ src/reducer.js
@@ -1,13 +1,21 @@
+let state = { count: 0 };
+
 function changeState(state, action) {
   switch (action.type) {
-    case "counter/increment":
+    case 'counter/increment':
       return { count: state.count + 1 };
     default:
       return state;
   }
 }
 
-let state = { count: 0 };
-let action = { type: "counter/increment" };
+function render() {
+  document.body.textContent = state.count;
+}
 
-changeState(state, action);
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+}
+// dispatch(action);
+render();
